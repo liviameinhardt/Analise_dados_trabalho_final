834132f6e76cb7c4f6b69f17881a23ef20580286
luizfernando608 <ziulluz@gmail.com>
2020-12-06 17:23:55 -0300

    Adicionando teste para função create_df do arquivo de_connection

--- testes_base_de_dados_e_conexoes.py
+++ testes_base_de_dados_e_conexoes.py
@@ -7,7 +7,6 @@ import pandas as pd
 print('-------------------------------------------')
 
 from Base_de_dados_conexoes_e_limpeza import db_connection
-from Base_de_dados_conexoes_e_limpeza import exceptions
 
 class testes_db_connection(unittest.TestCase):
     @classmethod
@@ -23,6 +22,7 @@ class testes_db_connection(unittest.TestCase):
         self.database = 'fgv-db'
         self.username = 'student'
         self.password = '@dsInf123'
+        self.connection_class = db_connection.create_connection(self.server, self.database, self.username, self.password)
         print('setUp')
 
     def tearDown(self):
@@ -42,21 +42,21 @@ class testes_db_connection(unittest.TestCase):
             db_connection.create_connection('tcp:fgv-db-server.database.windows.net,A123', self.database, self.username, self.password)
             
         with self.assertRaises(TypeError):
-            connection_class = db_connection.create_connection(121234, 546, 515, 9873)
-            connection_class = db_connection.create_connection(self.server, 0.5, self.username, self.password)
-            connection_class = db_connection.create_connection(self.server, self.database, self.username, 805)
-            connection_class = db_connection.create_connection(5, self.database, -12, self.password)
-            connection_class = db_connection.create_connection(5.8, self.database, -18, self.password)
-            connection_class = db_connection.create_connection(self.server, 5.9, -12, 27)
-            connection_class = db_connection.create_connection([1,2,4], self.database, self.username, self.password)
-            connection_class = db_connection.create_connection(self.server, dict(1,2,4), self.username, self.password)
-            connection_class = db_connection.create_connection(('1','2','3','4'), ('1','2','3','4'), ('1','2','3','4'), ('1','2','3','4'))
-            connection_class = db_connection.create_connection(dict('1','3','4'), dict('1','3','4'), dict('1','3','4'), dict('1','3','4'))
-            connection_class = db_connection.create_connection(['1','3','4'],['1','2', '3'], ['1','2', '3'], ['1','2', '3'])
-            connection_class = db_connection.create_connection(self.server, self.database, True, self.password)
-            connection_class = db_connection.create_connection(False, self.database, self.username, self.password)
-            connection_class = db_connection.create_connection(False, False, False, False)
-            connection_class = db_connection.create_connection(True, True, True, True)
+            db_connection.create_connection(121234, 546, 515, 9873)
+            db_connection.create_connection(self.server, 0.5, self.username, self.password)
+            db_connection.create_connection(self.server, self.database, self.username, 805)
+            db_connection.create_connection(5, self.database, -12, self.password)
+            db_connection.create_connection(5.8, self.database, -18, self.password)
+            db_connection.create_connection(self.server, 5.9, -12, 27)
+            db_connection.create_connection([1,2,4], self.database, self.username, self.password)
+            db_connection.create_connection(self.server, dict(1,2,4), self.username, self.password)
+            db_connection.create_connection(('1','2','3','4'), ('1','2','3','4'), ('1','2','3','4'), ('1','2','3','4'))
+            db_connection.create_connection(dict('1','3','4'), dict('1','3','4'), dict('1','3','4'), dict('1','3','4'))
+            db_connection.create_connection(['1','3','4'],['1','2', '3'], ['1','2', '3'], ['1','2', '3'])
+            db_connection.create_connection(self.server, self.database, True, self.password)
+            db_connection.create_connection(False, self.database, self.username, self.password)
+            db_connection.create_connection(False, False, False, False)
+            db_connection.create_connection(True, True, True, True)
 
         
     
@@ -70,6 +70,26 @@ class testes_db_connection(unittest.TestCase):
         self.assertIsInstance(db_connection.create_df('ufc.ufc_master',cursor),pd.DataFrame)
         self.assertIsInstance(db_connection.create_df('ufc.ufc_most_recent_event',cursor),pd.DataFrame)
         self.assertIsInstance(db_connection.create_df('ufc.ufc_upcoming_event',cursor),pd.DataFrame)
+
+        with self.assertRaises(TypeError):
+            db_connection.create_df(1234,cursor)
+            db_connection.create_df(['fifa.fifa_player'],cursor)
+            db_connection.create_df(dict('fifa_player'), cursor)
+            db_connection.create_df(tuple('fifa_player'), cursor)
+            db_connection.create_df(1234,1234)
+            db_connection.create_df(['fifa.fifa_player'],'pyodbc.Cursor')
+        with self.assertRaises(db_connection.invalid_table_name):
+            db_connection.create_df('PES.PES_player',cursor)
+            db_connection.create_df('Bomba_Patch2021.Bomba_Patch_player',cursor)
+            db_connection.create_df('wwe.ufc_upcoming_event',cursor)
+            db_connection.create_df('ufc.xfactor_most_recent_event',cursor)
+            db_connection.create_df('covid.covid_impact_on_aeroporto_traffic',cursor)
+            
+            
+            
+
+
+
     
         
     
