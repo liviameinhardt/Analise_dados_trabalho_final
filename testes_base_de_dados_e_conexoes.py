4b422df60d8a215a7522249c55aeb9f0299547f0
luizfernando608 <ziulluz@gmail.com>
2020-12-06 10:21:26 -0300

    Adicionando o arquivo de teste em local temporário

---
+++ testes_base_de_dados_e_conexoes.py
@@ -0,0 +1,34 @@
+import unittest
+import os
+import sys
+
+#sys.path.insert(0, os.path.abspath('..'))
+print('-------------------------------------------')
+
+#from Base_de_dados_conexoes_e_limpeza.db_connection import *
+
+'''
+class testes_db_connection(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print('setupClass\n')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('teardownClass')
+
+    def setUp(self):
+        print('setUp')
+        self.emp_1 = Employee('Corey', 'Schafer',50000)
+
+    def tearDown(self):
+        print('tearDown\n')
+
+    def test_create_conection(self):
+        print('test_email')
+        
+        self.assertEqual(self.emp_1.email,'Corey.Schafer@email.com')
+    
+'''
+#if __name__ == '__main__':
+#    unittest.main()
