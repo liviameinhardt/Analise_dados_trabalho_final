ca8f608c47a57733a2cca0143f15432e043a3fc6
João Pedro Donasolo <jpdonasolo@gmail.com>
2020-12-07 16:23:47 -0300

    Transformado em classe

--- Base_de_dados_conexoes_e_limpeza/db_cleaning_airport.py
+++ Base_de_dados_conexoes_e_limpeza/db_cleaning_airport.py
@@ -1,28 +1,34 @@
 import pandas as pd
 
+class Limpador:
 
-def drop_cols(df):
-    columns={'AggregationMethod', 'Unnamed: 0', 'Version', 'Centroid', 'Geography'}
-    df.drop(columns=columns, inplace=True)
-    return df
+    @classmethod
+    def drop_cols(cls, df):
+        columns={'AggregationMethod', 'Unnamed: 0', 'Version', 'Centroid', 'Geography'}
+        df.drop(columns=columns, inplace=True)
+        return df
 
-def create_datetime_cols(df):
-    df.loc[:, 'Date'] = pd.to_datetime(df['Date'])
-    df.loc[:, 'Year'] = df['Date'].dt.year
-    df.loc[:, 'Month'] = df['Date'].dt.month
-    df.loc[:, 'Day'] = df['Date'].dt.day
-    return df
+    @classmethod
+    def create_datetime_cols(cls, df):
+        df.loc[:, 'Date'] = pd.to_datetime(df['Date'])
+        df.loc[:, 'Year'] = df['Date'].dt.year
+        df.loc[:, 'Month'] = df['Date'].dt.month
+        df.loc[:, 'Day'] = df['Date'].dt.day
+        return df
 
-def standardize_country_names(df):
-    df.loc[:, 'Country'].replace('United States of America (the)', 'United States', inplace=True)
-    return df
+    @classmethod
+    def standardize_country_names(cls, df):
+        df = df.copy()
+        df.loc[:, 'Country'].replace('United States of America (the)', 'United States', inplace=True)
+        return df
 
-def clean_dataframe(df):
-    df = drop_cols(df.copy())
-    df = create_datetime_cols(df.copy())
-    df = standardize_country_names(df.copy())
-    return df
+    @classmethod
+    def clean_dataframe(cls, df):
+        df = cls.drop_cols(df.copy())
+        df = cls.create_datetime_cols(df.copy())
+        df = cls.standardize_country_names(df.copy())
+        return df
 
 if __name__ == '__main__':
     df = pd.read_csv('../Dados/covid_airport.csv')
-    df = clean_dataframe(df)
+    df = Limpador.clean_dataframe(df)
